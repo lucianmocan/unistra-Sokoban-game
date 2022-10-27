d5650d1540e408b79640cc3eeff273dc04988330
Lucian Mocan <lucian.mocan@etu.unistra.fr>
2022-10-27 17:12:21 +0200

    move_player() updated and now the player can move the box and both respect the game's walls.

--- player.c
+++ player.c
@@ -9,48 +9,81 @@ Grid move_player(Grid grid, enum Direction direction){
 
     switch(direction){
         case TOP: {
-            enum CaseType tmp = grid->game_grid[y-1][x];
-            printf("%c\n", tmp);
-            if (!(tmp == WALL || tmp == BOX)){
-                grid->game_grid[y-1][x] = PLAYER;
-                grid->game_grid[y][x] = NONE;
-                grid->player.y = y-1;
+            enum CaseType tmp_1 = grid->game_grid[y-1][x];
+            enum CaseType tmp_2 = grid->game_grid[y-2][x];
+            printf("%c\n", tmp_1);
+            if (!(tmp_1 == WALL)){
+                if (tmp_1 == BOX){
+                    if(tmp_2 != WALL){
+                        grid->game_grid[y-2][x] = BOX;
+                    }
+                }
+                if (!(tmp_2 == WALL && tmp_1 == BOX)){
+                    grid->game_grid[y-1][x] = PLAYER;
+                    grid->game_grid[y][x] = NONE;
+                    grid->player.y = y-1;
+                }
             }
             break;
         }
         case BOTTOM: {
-            enum CaseType tmp = grid->game_grid[y+1][x];
-            printf("%c\n", tmp);
-            if (!(tmp == WALL || tmp == BOX)){
-                grid->game_grid[y+1][x] = PLAYER;
-                grid->game_grid[y][x] = NONE;
-                grid->player.y = y+1;
+            enum CaseType tmp_1 = grid->game_grid[y+1][x];
+            enum CaseType tmp_2 = grid->game_grid[y+2][x];
+            printf("%c\n", tmp_1);
+            if (!(tmp_1 == WALL)){
+                if (tmp_1 == BOX){
+                    if(tmp_2 != WALL){
+                        grid->game_grid[y+2][x] = BOX;
+                    }
+                }
+                if (!(tmp_2 == WALL && tmp_1 == BOX)){
+                    grid->game_grid[y+1][x] = PLAYER;
+                    grid->game_grid[y][x] = NONE;
+                    grid->player.y = y+1;
+                }
             }
             break;
         }
         case LEFT: {
-            enum CaseType tmp = grid->game_grid[y][x-1];
-            printf("%c\n", tmp);
-            if (!(tmp == WALL || tmp == BOX)){
-                grid->game_grid[y][x-1] = PLAYER;
-                grid->game_grid[y][x] = NONE;          
-                grid->player.x = x-1;
+            enum CaseType tmp_1 = grid->game_grid[y][x-1];
+            enum CaseType tmp_2 = grid->game_grid[y][x-2];
+            printf("%c\n", tmp_1);
+            if (!(tmp_1 == WALL)){
+                if (tmp_1 == BOX){
+                    if(tmp_2 != WALL){
+                        grid->game_grid[y][x-2] = BOX;
+                    }
+                }
+                if (!(tmp_2 == WALL && tmp_1 == BOX)){
+                    grid->game_grid[y][x-1] = PLAYER;
+                    grid->game_grid[y][x] = NONE;
+                    grid->player.x = x-1;
+                }
             }
             break;
         }
         case RIGHT: {
-            enum CaseType tmp = grid->game_grid[y][x+1];
-            printf("%c\n", tmp);
-            if (!(tmp == WALL || tmp == BOX)){
-                grid->game_grid[y][x+1] = PLAYER;
-                grid->game_grid[y][x] = NONE;
-                grid->player.x = x+1;
+            enum CaseType tmp_1 = grid->game_grid[y][x+1];
+            enum CaseType tmp_2 = grid->game_grid[y][x+2];
+            printf("%c\n", tmp_1);
+            if (!(tmp_1 == WALL)){
+                if (tmp_1 == BOX){
+                    if(tmp_2 != WALL){
+                        grid->game_grid[y][x+2] = BOX;
+                    }
+                }
+                if (!(tmp_2 == WALL && tmp_1 == BOX)){
+                    grid->game_grid[y][x+1] = PLAYER;
+                    grid->game_grid[y][x] = NONE;
+                    grid->player.x = x+1;
+                }
             }
             break;
         }
     }    
     return grid;    
 }
+
 
 // int main(){
 
