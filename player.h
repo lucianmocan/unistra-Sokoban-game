00d97d3b9431b50521f29580a249a09f9b2ae7e8
lucianMD <lucian4forever@gmail.com>
2022-11-15 13:27:14 +0100

    Doxygen comments in player.h && added new field in StrGoals, to store the actual size of the array.

--- player.h
+++ player.h
@@ -4,30 +4,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @enum Direction
+ * @brief Des caractères représentant une certaine direction.
+ */
 enum Direction {
-    TOP = 'w',
-    LEFT = 'a',
-    RIGHT = 'd',
-    BOTTOM = 's'
+    TOP = 'w', ///< vers le haut
+    LEFT = 'a', ///< vers la gauche
+    RIGHT = 'd', ///< vers la droite
+    BOTTOM = 's' ///< vers le bas
 };
 
+/** 
+ * @struct Player player.h
+ * @brief Une structure représentant les coordonées d'un joueur.
+*/
 typedef struct Player{
-    int x;
-    int y;
+    int x; ///< la coordonnée x
+    int y; ///< la coordonnée y
 } Player;
 
+/** 
+ * @struct Goal player.h
+ * @brief Une structure représentant les coordonées d'une cible.
+*/
 typedef struct Goal{
-    int x;
-    int y;
+    int x; ///< la coordonnée x
+    int y; ///< la coordonnée y
 } Goal;
 
+/** 
+ * @struct StrGoals player.h
+ * @brief Une structure contenant un tableau des cibles
+ * le nombre d'éléments présents et la taille allouée.
+*/
 typedef struct StrGoals{
-    Goal* arrayOfGoals;
-    int size;
+    Goal* arrayOfGoals; ///< pointeur vers un tableau des cibles
+    int count; ///< le nombre d'éléments stockés dans le tableau
+    int size; ///< la taille allouée du tableau arrayOfGoals
 } StrGoals;
 
+/** 
+ * @struct Point player.h
+ * @brief Une structure représentant les coordonées d'un point.
+*/
 typedef struct Point{
-    int x;
-    int y;
+    int x; ///< la coordonnée x
+    int y; ///< la coordonnée y
 } Point;
 #endif
