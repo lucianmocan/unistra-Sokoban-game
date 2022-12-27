ed8fbcc6980c514f24732fd6a098a896aa491eb9
Lucian MOCAN <lucian.mocan@etu.unistra.fr>
2022-12-27 16:34:01 +0200

    edited main to use with sdl display, temporary

--- main.c
+++ main.c
@@ -8,30 +8,50 @@ int main(){
 	// call function to initialize SDL2 window
 	sdl_init();
 	
-	Grid grid = init_level("level1.txt");
+	Grid grid = init_level("level2.txt");
 	// display_level(grid);
 	display_sdl2(grid);
 
 	bool run = true;
 	while(run){
-		char entry = fgetc(stdin);
-		switch(entry){
-			case 'q' :{
+	// 	char entry = fgetc(stdin);
+	// 	switch(entry){
+	// 		case 'q' :{
+	// 			run = false;
+	// 			break;
+	// 		}
+	// 		default: {
+	// 			if (entry == 'a' || entry == 'w' || entry == 's' || entry == 'd'){
+	// 				grid = move_player(grid, entry);
+	// 				// display_level(grid);
+	// 				display_sdl2(grid);
+	// 			}
+	// 			// printf("targets found: %d\n", grid->covered_targets);
+	// 			if (checkIfDone(grid)){
+	// 				run = false;
+	// 				freeGrid(grid);
+	// 			}
+	// 			break;
+	// 		}
+	// 	}
+	// }
+		enum Event event = event_sdl2();
+		switch(event){
+			case Quit: {
 				run = false;
 				break;
 			}
 			default: {
-				if (entry == 'a' || entry == 'w' || entry == 's' || entry == 'd'){
-					grid = move_player(grid, entry);
-					// display_level(grid);
-					display_sdl2(grid);
+				if (event != None){
+						enum Direction entry = event_to_direction(event);
+						printf("%c\n", entry);
+						grid = move_player(grid, entry);
+						display_sdl2(grid);
 				}
-				// printf("targets found: %d\n", grid->covered_targets);
-				if (checkIfDone(grid)){
-					run = false;
-					freeGrid(grid);
-				}
-				break;
+				// if (checkIfDone(grid)){
+				// 	run = false;
+				// 	freeGrid(grid);
+				// }
 			}
 		}
 	}
